196eae47e66d8ec15396a6052c0b392f8d7f3b6d
ravindra <sirviravindra609@gmail.com>
2026-03-10 23:22:05 +0530

    Updated the Corect Shacn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Mono, Plus_Jakarta_Sans, Geist } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
-const jakartaSans = Plus_Jakarta_Sans({
-  variable: "--font-jakarta-sans",
-  subsets: ["latin"],
-});
-
-const plexMono = IBM_Plex_Mono({
-  variable: "--font-plex-mono",
-  weight: ["400", "500"],
-  subsets: ["latin"],
-});
+const geistSans = Geist({ subsets: ["latin"], variable: "--font-sans" });
+const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-mono" });
 
 export const metadata: Metadata = {
   title: "UMIS Software",
@@ -27,9 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html
+      lang="en"
+      className={cn("font-sans", geistSans.variable, geistMono.variable)}
+    >
       <body
-        className={`${jakartaSans.variable} ${plexMono.variable} bg-background text-foreground antialiased`}
+        className="bg-background text-foreground antialiased"
       >
         {children}
       </body>
